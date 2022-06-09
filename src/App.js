38d3a1aff0c489a022005bab17111941ad81b96a
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-06-09 15:26:33 +0300

    feat: added five days weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Moment from 'moment';
 import { AiFillCloud } from 'react-icons/ai';
 import { BsFillSunFill } from 'react-icons/bs';
 import { FiWind } from 'react-icons/fi';
@@ -13,6 +14,7 @@ const App = () => {
 
   const [search, setSearch] = useState('');
   const [weather, setWeather] = useState({});
+  const [week, setWeek] = useState([]);
   const [fetching, setFetching] = useState(true)
 
   // Current position
@@ -26,13 +28,18 @@ const App = () => {
 
   const [latitude, setLatitude] = useState(lat);
   const [longitude, setLongitude] = useState(lon);
-  
+
 
   useEffect(() => {
     axios(`${api.base}weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}`)
       .then(res => {
         setWeather(res.data)
         setFetching(false)
+      })
+    axios(`${api.base}forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}`)
+      .then(res => {
+        const filter = res.data.list.filter((f) => f.dt_txt.includes('15:00:00'))
+        setWeek(filter)
       })
   }, [latitude, longitude])
 
@@ -52,7 +59,7 @@ const App = () => {
       'September', 'October', 'November', 'December'];
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    let day = days[d.getDay()];
+    let day = days[d.getDay() -1];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
@@ -76,7 +83,7 @@ const App = () => {
             onKeyDown={fetchWeather}
             type='search'
             className='search-body'
-            placeholder='Serach...' />
+            placeholder='Search...' />
         </div>
         <div className='app-wrapper'>
           <div className='location-box'>
@@ -86,8 +93,9 @@ const App = () => {
           <div className='weather-box'>
             <div className='weather-main'>
               <div className='weather-main__cloud'>
-                {(weather.weather[0].description) !== 'clear sky' ?
-                  <AiFillCloud size={50} /> : <BsFillSunFill size={50} />}
+                {/* {(weather.weather[0].description) !== 'clear sky' ?
+                  <AiFillCloud size={50} /> : <BsFillSunFill size={50} />} */}
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt='weather icon' />
                 <span>{weather.weather[0].description}</span>
               </div>
               <div className='weather-main__wind'>
@@ -100,6 +108,20 @@ const App = () => {
               <span>Feels like: {Math.round(weather.main.feels_like)}℃</span>
               <span>Max: {Math.round(weather.main.temp_max)}℃ Min: {Math.round(weather.main.temp_min)}℃</span>
             </div>
+          </div>
+          <div className='weather-week'>
+            {week.map((item) => {
+              return (
+                <div key={item.dt} className='weather-week__item'>
+                  <div className='week-item__day'>
+                    {Moment(item.dt_txt).format('dddd')}
+                  </div>
+                  <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`} alt='weather icon' />
+                  <span>{item.weather[0].main}</span>
+                  <span>{Math.round(item.main.temp)}℃</span>
+                </div>
+              )
+            })}
           </div>
         </div>
       </main>
